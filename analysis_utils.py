49a673bfe384f2c19a2abfabec23a526953aec0a
jackieQizhu <jackiecococa@gmail.com>
2021-07-08 22:49:05 +0800

    1. Adds some usefule tools 2. Trying to change the logr

--- analysis_utils.py
+++ analysis_utils.py
@@ -1,14 +1,16 @@
 from pandas import Series
+import numpy as np
+import pandas as pd
 # create a differenced series
 def difference(dataset, interval=1):
-    diff = list()
+    diff = list([np.nan])
     for i in range(interval, len(dataset)):
         value = dataset[i] - dataset[i - interval]
         diff.append(value)
     return Series(diff)
 
 def difference_rate(dataset, interval=1):
-    diff = list()
+    diff = list([np.nan])
     for i in range(interval, len(dataset)):
         value = (dataset[i] - dataset[i - interval])/dataset[i - interval]
         diff.append(value)
