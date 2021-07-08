e35eee571197e3fe880dffce11e53fcf00c9da92
jackieQizhu <jackiecococa@gmail.com>
2021-07-08 14:44:22 +0800

    1. Adds time calculation module for important sections 2. Modifies some small bugs in ga.py 3. Adds gitignore to eliminate useless files 4. Runs through the whole project.

--- ga.py
+++ ga.py
@@ -2,16 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def cal_pop_fitness(equation_inputs, pop):
     # Calculating the fitness value of each solution in the current population.
     # The fitness function calulates the sum of products between each input and its corresponding weight.
     # fitness = np.sum(pop*equation_inputs, axis=1)
     # pop size (8,6)
     # fitness size 8x1
-    logr = equation_inputs[:,0]
-    temp = pop@equation_inputs[:,1:7].T
-    fitness = np.sum(temp*logr, axis = 1)/np.sum(pop, axis = 1)
+    logr = equation_inputs[:, 0]
+    temp = pop @ equation_inputs[:, 1:7].T
+    fitness = np.sum(temp * logr, axis=1) / np.sum(pop, axis=1)
     return fitness
+
 
 def select_mating_pool(pop, fitness, num_parents):
     # Selecting the best individuals in the current generation as parents for producing the offspring of the next generation.
@@ -23,21 +25,23 @@ def select_mating_pool(pop, fitness, num_parents):
         fitness[max_fitness_idx] = -99999999999
     return parents
 
+
 def crossover(parents, offspring_size):
     offspring = np.empty(offspring_size)
     # The point at which crossover takes place between two parents. Usually, it is at the center.
-    crossover_point = np.uint8(offspring_size[1]/2)
+    crossover_point = np.uint8(offspring_size[1] / 2)
 
     for k in range(offspring_size[0]):
         # Index of the first parent to mate.
-        parent1_idx = k%parents.shape[0]
+        parent1_idx = k % parents.shape[0]
         # Index of the second parent to mate.
-        parent2_idx = (k+1)%parents.shape[0]
+        parent2_idx = (k + 1) % parents.shape[0]
         # The new offspring will have its first half of its genes taken from the first parent.
         offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
         # The new offspring will have its second half of its genes taken from the second parent.
         offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
     return offspring
+
 
 def mutation(offspring_crossover, num_mutations=1):
     mutations_counter = np.uint8(offspring_crossover.shape[1] / num_mutations)
@@ -51,54 +55,54 @@ def mutation(offspring_crossover, num_mutations=1):
             gene_idx = gene_idx + mutations_counter
     return offspring_crossover
 
-def cal_pop_fitness(equation_inputs, pop, opt = 0):
+
+def cal_pop_fitness(equation_inputs, pop, opt=0):
     # Calculating the fitness value of each solution in the current population.
     # Opt = 0 is the GAMSSR model
-    logr = equation_inputs[:,0] # n,
-    positions = pop@equation_inputs[:,1:].T
-    port_r = (positions*logr).astype(np.float64)
-    SSR = np.mean(port_r, axis = 1)/np.std(port_r, axis = 1)/(-np.sum(port_r[port_r<0]))
+    logr = equation_inputs[:, 0]  # n,
+    positions = pop @ equation_inputs[:, 1:].T
+    port_r = (positions * logr).astype(np.float64)
+    SSR = np.mean(port_r, axis=1) / np.std(port_r, axis=1) / (-np.sum(port_r[port_r < 0]))
     return SSR
 
-def GA_train(training_df, optimizing_selection=0, sol_per_pop=8, num_parents_mating=4, num_generations = 200):
+
+def GA_train(training_df, optimizing_selection=0, sol_per_pop=8, num_parents_mating=4, num_generations=200):
     """
     Genetic algorithm parameters:
         Mating pool size
         Population size
     """
-    #Inputs of the equation.
+    # Inputs of the equation.
     equation_inputs = training_df.values
     # Number of the weights we are looking to optimize.
-    num_weights = training_df.shape[1]-1
+    num_weights = training_df.shape[1] - 1
 
     # Defining the population size.
-    pop_size = (sol_per_pop,num_weights) 
+    pop_size = (sol_per_pop, num_weights)
     # The population will have sol_per_pop chromosome 
     # where each chromosome has num_weights genes.
-    
+
     # Creating the initial population.
     new_population = np.random.uniform(low=-1.0, high=1.0, size=pop_size)
     # print(new_population)
 
     best_outputs = []
-    
+
     for generation in range(num_generations):
-    #     print("Generation : ", generation)
+        #     print("Generation : ", generation)
         # Measuring the fitness of each chromosome in the population.
         fitness = cal_pop_fitness(equation_inputs, new_population, optimizing_selection)
 
         best_outputs.append(np.max(fitness))
 
         # Selecting the best parents in the population for mating.
-        parents = ga.select_mating_pool(new_population, fitness, 
-                                          num_parents_mating)
+        parents = select_mating_pool(new_population, fitness, num_parents_mating)
 
         # Generating next generation using crossover.
-        offspring_crossover = ga.crossover(parents,
-                                           offspring_size=(pop_size[0]-parents.shape[0], num_weights))
+        offspring_crossover = crossover(parents, offspring_size=(pop_size[0] - parents.shape[0], num_weights))
 
         # Adding some variations to the offspring using mutation.
-        offspring_mutation = ga.mutation(offspring_crossover, num_mutations=2)
+        offspring_mutation = mutation(offspring_crossover, num_mutations=2)
 
         # Creating the new population based on the parents and offspring.
         new_population[0:parents.shape[0], :] = parents
@@ -109,7 +113,7 @@ def GA_train(training_df, optimizing_selection=0, sol_per_pop=8, num_parents_mat
     fitness = cal_pop_fitness(equation_inputs, new_population, optimizing_selection)
     # Then return the index of that solution corresponding to the best fitness.
     best_match_idx = np.where(fitness == np.max(fitness))
-        
+
     plt.plot(best_outputs)
     plt.xlabel("Iteration")
     plt.ylabel('SSR ratio')
