cfb35ebbdd88d2b05d3cdc9c119e1bc59d510ff0
jackieQizhu <jackiecococa@gmail.com>
2021-07-13 00:58:39 +0800

    Refactors the logic for reading data

--- Utils/data_utils.py
+++ Utils/data_utils.py
@@ -2,15 +2,18 @@ import pandas as pd
 
 from pandas.testing import assert_series_equal
 
-def get_duka_data_df(path):
+def get_duka_data_df(path, symbol):
     """
       dukascopy数据应用时间为GMT时间
     """
+    assert (symbol.isupper()), "Argument symbol should be uppercase"
     df = pd.read_csv(path)
     df["datetime"] = pd.to_datetime(df["Gmt time"], format="%d.%m.%Y %H:%M:%S.000")
     # filters the weekends and holidays which have not trasactions.
-    df = df[df["Volume"]!=0] 
-    df = df[["datetime", "Open", "High", "Low", "Close", "Volume"]]
+    df = df[df["Volume"]!=0]
+    df["symbol"] = symbol
+    df = df[["symbol","datetime", "Open", "High", "Low", "Close", "Volume"]]
+    df.columns = ["symbol","datetime", "open", "high", "low", "close", "volume"]
     return df
 
 def get_fm_rc_data_df(path, drop_balance_info=True):
