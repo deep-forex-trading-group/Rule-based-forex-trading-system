31aef6e3108900d44a214a0454f28f698d0f63c3
jackieQizhu <jackiecococa@gmail.com>
2021-07-13 00:34:04 +0800

    Updates some data utils to read followme records and currency data

--- Utils/data_utils.py
+++ Utils/data_utils.py
@@ -1,7 +1,33 @@
 import pandas as pd
 
-def get_duka_df(path):
+from pandas.testing import assert_series_equal
+
+def get_duka_data_df(path):
+    """
+      dukascopy数据应用时间为GMT时间
+    """
     df = pd.read_csv(path)
     df["datetime"] = pd.to_datetime(df["Gmt time"], format="%d.%m.%Y %H:%M:%S.000")
+    # filters the weekends and holidays which have not trasactions.
+    df = df[df["Volume"]!=0] 
     df = df[["datetime", "Open", "High", "Low", "Close", "Volume"]]
     return df
+
+def get_fm_rc_data_df(path, drop_balance_info=True):
+    """
+    get the followme records data
+    """
+    rc_df = pd.read_csv(path)
+    # drop the balance infomation
+    if drop_balance_info:
+        rc_df = rc_df.dropna()
+    rc_df["OpenTime"] = pd.to_datetime(rc_df["OpenTime"])
+    rc_df["CloseTime"] = pd.to_datetime(rc_df["CloseTime"])
+    assert_series_equal(rc_df["FollowmeLots"], rc_df["BrokerLots"], check_names=False) 
+    rc_df = rc_df[['Symbol', 'Type', 'FollowmeLots', 
+              'OpenTime', 'CloseTime','OpenPrice', 'ClosePrice', 
+              'Profit', 'Pips']]
+    rc_df.columns = ['symbol', 'action', 'lots', 
+                'open_date', 'close_date','open_price', 'close_price', 
+                'profits', 'pips']
+    return rc_df
