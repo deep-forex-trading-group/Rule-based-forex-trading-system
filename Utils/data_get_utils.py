8a9e536de4a938ebb3d2e90160d560e59149d720
jackieQizhu <jackiecococa@gmail.com>
2021-07-15 02:32:54 +0800

    1. Adds the followme record module    Mod: Changes the way to generate record as yht_eur_bit_table.csv         And then use the analysis_fmrecord_with_bar_data.ipynb to analyze         combined with the input bar data 2. Adds some useful functions for getting data    TODO: to clarify the management of the utils 3. Modifies some dependencies of requirements.txt    pay attention to use pip-chill

--- Utils/data_get_utils.py
+++ Utils/data_get_utils.py
@@ -5,6 +5,19 @@ from datetime import timedelta
 from datetime import datetime
 from pandas.testing import assert_series_equal
 
+
+def calc_profit(rc_df, act_cname="action", open_price_cname="open_price", close_price_cname="close_price", 
+           lots_cname="lots", comm_cname="commission", swaps_cname="swaps", decimal_pips=10000):
+    rc_pips = np.where(rc_df[act_cname]=='buy', 
+                 (rc_df[close_price_cname]-rc_df[open_price_cname]) * decimal_pips, 
+                 (rc_df[open_price_cname]-rc_df[close_price_cname]) * decimal_pips)
+    np.testing.assert_array_almost_equal(np.array(rc_pips), np.array(rc_df["pips"]), decimal=1, verbose=True)
+    rc_profits_calc_itself_col_series = ((rc_pips)/decimal_pips)*rc_df[lots_cname]*decimal_pips + rc_df[comm_cname] + rc_df[swaps_cname]
+    np.testing.assert_array_almost_equal(np.array(rc_profits_calc_itself_col_series), np.array(rc_df["profits_calc"]), 
+                              decimal=1, verbose=True)
+    rc_df["profits_calc"] = rc_profits_calc_itself_col_series
+
+
 def get_duka_data_df(path, symbol):
     """
       dukascopy数据应用时间为GMT时间
@@ -19,7 +32,7 @@ def get_duka_data_df(path, symbol):
     df.columns = ["symbol","datetime", "open", "high", "low", "close", "volume"]
     return df
 
-def get_fm_rc_data_df(path, drop_balance_info=True, time_delta_hour=3):
+def get_fm_rc_data_df(path, drop_balance_info=True, time_delta_hour=3, decimal_pips=10000):
     """
      get the followme records data
     """
@@ -39,24 +52,25 @@ def get_fm_rc_data_df(path, drop_balance_info=True, time_delta_hour=3):
     rc_df.columns = ['symbol', 'action', 'lots', 
                 'open_date', 'close_date','open_price', 'close_price', 
                 'profits', 'pips','commission','swaps']
-    # filter the meaningless with 0 lots record
+    # filter the meaningless with 0 lots record, weekends and holidays
     rc_df = rc_df[rc_df["lots"] != 0]
     
     # caculate the profit based on the raw price variation data
-    rc_df["profits_calc"] = (rc_df["pips"]/10000)*rc_df["lots"]*100000 + rc_df["commission"] + rc_df["swaps"]
+    rc_df["profits_calc"] = (rc_df["pips"]/decimal_pips)*rc_df["lots"]*decimal_pips + rc_df["commission"] + rc_df["swaps"]
     
     # calculate the commision counted as the pips change
     rc_df["commission_by_pips"] = rc_df["commission"]/rc_df["lots"]/10
     
     # calculate the date variation for calculating swaps
     def calc_date_change(row):
-        open_date = row["open_date"].to_pydatetime().day
-        close_date = row["close_date"].to_pydatetime().day
-        return close_date - open_date
+        open_date = row["close_date"].to_pydatetime().replace(hour=0, minute=0, second=0)
+        close_date = row["open_date"].to_pydatetime().replace(hour=0, minute=0, second=0)
+        
+        return (open_date - close_date).days
         
     rc_df["date_var"] = rc_df[["open_date", "close_date"]].apply(calc_date_change, axis=1)
     
     # calculate the swaps by time variations and pips
-    rc_df["swap_by_time_variations_and_pips"] = np.where(rc_df["date_var"] != 0, rc_df["swaps"]/rc_df["date_var"]/rc_df["lots"]/10, 0)
+    rc_df["swap_per_date_var_and_pip"] = np.where(rc_df["date_var"] != 0, rc_df["swaps"]/rc_df["date_var"]/rc_df["lots"]/10, 0)
     
     return rc_df
