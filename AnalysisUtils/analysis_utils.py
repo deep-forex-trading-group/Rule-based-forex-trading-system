4818bbbe4cb27218c010d7649264677d8931386b
jackieQizhu <jackiecococa@gmail.com>
2021-08-13 23:42:40 +0800

    1. Adds the bbq_helper for the babyquant helper functions. 2. Adds some analysis steps.

--- AnalysisUtils/analysis_utils.py
+++ AnalysisUtils/analysis_utils.py
@@ -16,6 +16,17 @@ def difference_rate(dataset, interval=1):
         diff.append(value)
     return Series(diff)
 
-def reverse_df(df):
-    return df.reindex(index=df.index[::-1])
+def reverse_df(df, is_reindex=True):
+    # return df.reindex(index=df.index[::-1])
+    return df.iloc[::-1].reset_index(drop=True) if is_reindex else df.iloc[::-1]
+
+def calc_ssr(rt):
+    rt = rt.astype(np.float64)
+    ssr = np.mean(rt) / np.std(rt) / (-np.sum(rt[rt < 0])) * len(rt)
+    return ssr
+
+def calc_sr(rt):
+    rt = rt.astype(np.float64)
+    sr = np.mean(rt) / np.std(rt) * len(rt)
+    return sr
     
